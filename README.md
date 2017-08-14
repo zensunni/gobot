72aa59063a28b30fbce96cb63d04413101ad36a8
kirk <kirk.werklund@gmail.com>
2017-08-14 16:09:14 -0600

    Messing around with pygame gamepad functionality

--- README.md
+++ README.md
@@ -32,4 +32,21 @@ Possible Software
 * pypi.python.org/pypi/inputs
 * libsdl.org/languages.php
 * github.com/jkuhlmann/gainput
-* github.com/markusressel/raspyrfm-client
+
+## Gamepad input:
+
+* Language: python
+* Library: PyGame_SDL2, or maybe just straight-up pygame
+
+Notes:
+
+* It looks like everyone is using pygame's .joystick and/or .key api
+* PyGame_SDL2 is a reimplementation of the Pygame API using SDL2 and related libraries.
+
+Helpful sources:
+
+* PyGame_SDL2: https://github.com/renpy/pygame_sdl2
+* Pygame docs: http://www.pygame.org/docs
+* https://www.reddit.com/r/pygame/comments/1zgixs/xbox_360_controller_with_pygame/
+* https://yameb.blogspot.ca/2013/01/gamepad-input-in-python.html
+* http://robots.dacloughb.com/project-1/logitech-game-pad/
--- client.py
+++ client.py
@@ -1,36 +1,67 @@
 import socket
-import curses
+import pygame
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Start your engines!"
+# ====== Port connection setup
+#UDP_IP = "127.0.0.1"
+#UDP_PORT = 5005
+#MESSAGE = "Start your engines!"
+#print "UDP target IP:", UDP_IP
+#print "UDP target port:", UDP_PORT
+#print "message:", MESSAGE
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+#sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+# =======
+
+pygame.init()
+
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+
+clock = pygame.time.Clock()
+
+# [NCURSES INPUT] do not wait for input when calling getch
+#stdscr.nodelay(1)
+
+while True:
+    pygame.event.pump()
+    #for event in pygame.event.get():
+    print joystick.get_axis(0),  joystick.get_axis(1)
 
 
-def main(stdscr):
+    # [NCURSES INPUT] get keyboard input, returns -1 if none available
+    #c = stdscr.getch()
 
-    # do not wait for input when calling getch
-    stdscr.nodelay(1)
+    #if c == 27:
+        #break;
+    # pygame.key.get_press()
+
+#        stdscr.addstr(str(int(axis)))
+#        stdscr.refresh()
+    # return curser to start position
+#        stdscr.move(0, 0)
+
+    clock.tick(40)
 
 
-    while True:
-        # get keyboard input, returns -1 if none available
-        c = stdscr.getch()
-        if c != -1:
-            # print numeric value
+pygame.quit()
 
-            sock.sendto(str(c), (UDP_IP, UDP_PORT))
-            stdscr.addstr(str(c) + ' ')
-            stdscr.refresh()
-            # return curser to start position
-            stdscr.move(0, 0)
+# ====== Port connection actions
+#        if c != -1:
+#            if c == 27:
+#                break;
+#            # print numeric value
+#
+#            sock.sendto(str(c), (UDP_IP, UDP_PORT))
+#            stdscr.addstr(str(c) + ' ')
+#            stdscr.refresh()
+#            # return curser to start position
+#            stdscr.move(0, 0)
+# =======
 
-if __name__ == '__main__':
-    curses.wrapper(main)
+#if __name__ == '__main__':
+#    curses.wrapper(main)
 
+# Sources:
+  # https://stackoverflow.com/questions/39817549/pygame-joystick-get-axis-always-returns-zero
